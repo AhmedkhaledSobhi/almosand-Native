72e7e8b9043804a7450891913e51346637e165f9
Ahmed <Ahmed.Khaled01144271924@gmail.com>
2024-04-24 14:28:23 +0200

    update style Confirm the payment process

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+document.getElementById("addPaymentProcess").addEventListener("click", function() {
+    var newDiv = document.createElement("div");
+    // Add content inside the new div
+    newDiv.innerHTML =` <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <div>
+                <label for="paymentMethod">طريقة الدفع او السداد</label>
+                <select class="select-user w-100 px-2" id="paymentMethod" aria-label="">
+                    <option class="" selected> كاش </option>
+                    <option value="1"> تحويل بنكي </option>
+                    <option value="2"> شيك </option>
+                    <option value="3"> ماستر كارد </option>
+                    <option value="4"> Card </option>
+                    <option value="5"> رصيد السفظة Wallct </option>
+                    <option value="6"> اخري </option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div>
+                <label for="total">الاجمالي</label>
+                <input class="px-2 w-100" type="number" name="total" id="total" placeholder="الاجمالي">
+            </div>
+        </div>
+    </div> `;
+
+
+
+
+
+document.getElementById("addChild").appendChild(newDiv);
+});
+
