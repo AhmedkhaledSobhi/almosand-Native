48b58c2abc7d9ce2c04cbccca85ed0e9d07e91ef
Ahmed <Ahmed.Khaled01144271924@gmail.com>
2024-05-02 01:17:18 +0300

    update file main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,48 @@
 
+(() => {
+    const folders = document.querySelectorAll('.folder');
 
-
-const folders = document.querySelectorAll('.folder');
-
-for (const folder of folders) {
-    folder.onclick = () => {
-        folder.classList.toggle('active');
-        const target_list = folder.nextElementSibling;
-        target_list.style.maxHeight = target_list.style.maxHeight ? null : "100vh";
+    for (const folder of folders) {
+        folder.onclick = () => {
+            folder.classList.toggle('active');
+            const target_list = folder.nextElementSibling;
+            target_list.style.maxHeight = target_list.style.maxHeight ? null : "100vh";
+        }
     }
-}
 
+})();
 
+(()=>{
 
-
-
-
-
-document.getElementById("addPaymentProcess").addEventListener("click", function () {
-    var newDiv = document.createElement("div");
-    // Add content inside the new div
-    newDiv.innerHTML = ` <hr>
-    <div class="row">
-        <div class="col-md-6">
-            <div>
-                <label for="paymentMethod">طريقة الدفع او السداد</label>
-                <select class="select-user w-100 px-2" id="paymentMethod" aria-label="">
-                    <option class="" selected> كاش </option>
-                    <option value="1"> تحويل بنكي </option>
-                    <option value="2"> شيك </option>
-                    <option value="3"> ماستر كارد </option>
-                    <option value="4"> Card </option>
-                    <option value="5"> رصيد السفظة Wallct </option>
-                    <option value="6"> اخري </option>
-                </select>
+    document.getElementById("addPaymentProcess").addEventListener("click", function () {
+        var newDiv = document.createElement("div");
+        // Add content inside the new div
+        newDiv.innerHTML = ` <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <div>
+                    <label for="paymentMethod">طريقة الدفع او السداد</label>
+                    <select class="select-user w-100 px-2" id="paymentMethod" aria-label="">
+                        <option class="" selected> كاش </option>
+                        <option value="1"> تحويل بنكي </option>
+                        <option value="2"> شيك </option>
+                        <option value="3"> ماستر كارد </option>
+                        <option value="4"> Card </option>
+                        <option value="5"> رصيد السفظة Wallct </option>
+                        <option value="6"> اخري </option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="col-md-6">
-            <div>
-                <label for="total">الاجمالي</label>
-                <input class="px-2 w-100" type="number" name="total" id="total" placeholder="الاجمالي">
+            <div class="col-md-6">
+                <div>
+                    <label for="total">الاجمالي</label>
+                    <input class="px-2 w-100" type="number" name="total" id="total" placeholder="الاجمالي">
+                </div>
             </div>
-        </div>
-    </div> `;
+        </div> `;
+    
+        document.getElementById("addChild").appendChild(newDiv);
+    });
 
-
-
-
-
-    document.getElementById("addChild").appendChild(newDiv);
-});
-
-
-
-
+})()
 
