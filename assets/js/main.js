3b52929e076c45687a0443abdbf56164ed4cfa96
Ahmed <Ahmed.Khaled01144271924@gmail.com>
2024-05-07 01:06:35 +0300

    add page invoice and qr code

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,77 @@
 
 })()
 
+
+
+
+const url = 'https://dashboard.almosand.com/api/pos-invoices';
+
+const headers=new Headers;
+headers.append("Custom-Header", 'YourHeaderValue');
+
+
+// Define the invoice data
+const invoiceData = {
+    "customer_id": 33,
+    "tid": "#R-1000",
+    "refer": "#R-1000",
+    "invoicedate": "2024-02-24",
+    "invoiceduedate": "2024-02-24",
+    "recur_after": "",
+    "sub": null,
+    "notes": "Test",
+    "subtotal": 1500.00,
+    "shipping": 50.00,
+    "tax": 150.00,
+    "discount": 0,
+    "discount_rate": 0,
+    "after_disc": 0,
+    "currency": "EGP",
+    "total": 1700.00,
+    "tax_format": "FLAT",
+    "discount_format": "percentage",
+    "ship_tax": 7.5,
+    "ship_tax_type": "percentage",
+    "ship_rate": "15",
+    "term_id": null,
+    "tax_id": 1,
+    "p": null,
+    "user_id": 31,
+    "product_id": [31],
+    "product_name": ["Shawrma"],
+    "code": ["332"],
+    "product_qty": [5],
+    "product_price": [10],
+    "product_tax": [1.2],
+    "product_discount": [0],
+    "product_subtotal": [53.44],
+    "total_tax": [6],
+    "total_discount": [0],
+    "product_description": ["TEST"],
+    "unit": ["Pack"],
+    "serial": ["223-444"],
+    "unit_m": ["Pack"],
+    "p_amount": [1700.00],
+    "p_method": ["cash"],
+    "p_account": [220],
+    "b_change": [300]
+};
+
+fetch(url, {
+    method: 'POST',
+   
+    body: JSON.stringify(invoiceData),
+})
+.then(response => {
+    console.log(response);
+    if (!response.ok) {
+        throw new Error('Network response was not ok' );
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Success:', data);
+})
+.catch(error => {
+    console.error('Error:', error);
+});
